f2bd9860a5674f17f58fd53a95175b0e56e09857
thedimas3007 <51395517+thedimas3007@users.noreply.github.com>
2021-04-02 12:29:30 +0300

    removed mods command and changed list of rules

--- src/main/ThedimasPlugin.java
+++ src/main/ThedimasPlugin.java
@@ -10,44 +10,24 @@ public class ThedimasPlugin extends Plugin{
     String rules_ru = new String("1. Не спамить/флудить в чат\n"
                             + "2. Не оскорблять других участников сервера\n"
                             + "3. Не быть грифером\n"
-                            + "[scarlet]4. Не строить NSFW (18+) схемы[]\n\n"
-                            + "[accent]1 нарушение = 1 варн + кик\n"
-                            + "3 варна = бан\n"
-                            + "NFSW = бан[]");
+                            + "[scarlet]4. Не строить NSFW (18+) схемы[]\n");
     
-    String welcome_ru = new String("Привет, путник!\n"
+    String welcome_ru = new String("[white]Привет, путник!\n"
                                  + "Добро пожаловать на сеть серверов от thedimas!\n"
-                                 + "Вот правила и наказания:\n"
-                                 + rules_ru + "\n"
+                                 + "Вот правила:\n"
+                                 + "[accent]" + rules_ru + "[white]\n"
                                  + "\nЕсли ты их забыл, то можешь ввести комманду [accent]/rules");
 
     String rules_en = new String("1. Don't spam/flood in the chat\n"
                                + "2. Don't insult another players\n"
                                + "3. Don't grief\n"
-                               + "[scarlet]4. Don't build NSFW (18+) schemes[]\n\n"
-                               + "[accent]1 breach = 1 warn + kick\n"
-                               + "3 warns = ban\n"
-                               + "NFSW = ban[]");
+                               + "[scarlet]4. Don't build NSFW (18+) schemes[]\n");
 
-    String welcome_en = new String("Hi, traveller!\n"
-                                 + "Welcome back to thedimas' servers!\n"
-                                 + "Here are the rules and punishments:\n"
-                                 + rules_en + "\n"
+    String welcome_en = new String("[white]Hi, traveller!\n"
+                                 + "Welcome to thedimas' servers!\n"
+                                 + "Here are the rules:\n"
+                                 + "[accent]" + rules_en + "[]\n"
                                  + "\nIf you forgot them, you can type [accent]/rules[] command");
-    
-    String mods_ip = new String("95.217.226.152:26233");
-    
-    String mods_ru = new String("[accent]Нужные моды:\n"
-                              + "[green]Braindustry\n"
-                              + "[green]BetaMindy\n"
-                              + "[green]ReVision\n\n"
-                              + "[accent]IP сервера[]: [cyan]" + mods_ip);
-
-    String mods_en = new String("[accent]Necessary mods:\n"
-                              + "[green]Braindustry\n"
-                              + "[green]BetaMindy\n"
-                              + "[green]ReVision\n\n"
-                              + "[accent]Server IP[]: [cyan]" + mods_ip);
             
     //called when game initializes
     @Override
@@ -57,31 +37,30 @@ public class ThedimasPlugin extends Plugin{
             Log.info(event.player.name + " has joined the server");
             Log.info("\tLocale: " + event.player.locale);
             Log.info("\tIP: " + event.player.con.address);
-            if(event.player.locale.startsWith("ru") || event.player.locale.startsWith("uk")) {
+            Call.sendMessage("[lime]+ [accent]" + event.player.name + "[lime] присоединился");
+            /*if(event.player.locale.startsWith("ru") || event.player.locale.startsWith("uk")) {
                 Call.infoMessage(event.player.con, welcome_ru);
             } else {
                 Call.infoMessage(event.player.con, welcome_en);
-            } 
+            }*/
+        });
+        Events.on(EventType.PlayerLeave.class, event -> { // called when player leave
+            Call.sendMessage("[scarlet]- [accent]" + event.player.name + "[scarlet] вышел");
+            Log.info(event.player.name + " has disconnected from the server");
+
         });
     }
     @Override
     public void registerClientCommands(CommandHandler handler) {
-        handler.<Player>register("rules", "Get list of rules.", (args, player) -> {
+        /*handler.<Player>register("rules", "Get list of rules.", (args, player) -> {
             if(player.locale.startsWith("ru") || player.locale.startsWith("uk")) {
                 player.sendMessage(rules_ru);
             } else {
                 player.sendMessage(rules_en);
             } 
-        });
+        });*/
         handler.<Player>register("hub", "Connect to HUB.", (args, player) -> {
             Call.connect(player.con, "95.217.226.152", 26160);
-        });
-        handler.<Player>register("mods", "Get list of mods.", (args, player) -> {
-            if(player.locale.startsWith("ru") || player.locale.startsWith("uk")) {
-                player.sendMessage(mods_ru);
-            } else {
-                player.sendMessage(mods_en);
-            } 
         });
     }
 }
