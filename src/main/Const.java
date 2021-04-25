1330b864b49ba07eac32e841c862a04d652afee3
MrGamer2k17 <yar300102@gmail.com>
2021-04-25 17:28:40 +0300

    separated constants

---
+++ src/main/Const.java
@@ -0,0 +1,41 @@
+package main;
+
+public class Const {
+    public static final String FORMAT = "<%0> %1[white]: %2";
+
+    public static final String RULES_UK = "1. Не спамити/флудити в чат\n"
+            + "2. Не ображати інших учасників сервера\n"
+            + "3. Не бути гріфером\n"
+            + "[scarlet]4. Не будувати NSFW (18+) схеми[]\n";
+
+    public static final String WELCOME_UK = "[white]Привіт, подорожній!\n"
+            + "Ласкаво просимо на мережу серверів від thedimas!\n"
+            + "Ось правила:\n"
+            + "[accent]" + RULES_UK + "[white]\n"
+            + "\nЯкщо ти їх забув, то можеш ввести комманду [accent]/rules[]\n"
+            + "Докладні правила ти можеш знайти на нашому Discord сервері";
+
+    public static final String RULES_RU = "1. Не спамить/флудить в чат\n"
+            + "2. Не оскорблять других участников сервера\n"
+            + "3. Не быть грифером\n"
+            + "[scarlet]4. Не строить NSFW (18+) схемы[]\n";
+
+    public static final String WELCOME_RU = "[white]Привет, путник!\n"
+            + "Добро пожаловать на сеть серверов от thedimas!\n"
+            + "Вот правила:\n"
+            + "[accent]" + RULES_RU + "[white]\n"
+            + "\nЕсли ты их забыл, то можешь ввести комманду [accent]/rules[]\n"
+            + "Подробные правила ты можешь найти на нашем Discord сервере";
+
+    public static final String RULES_EN = "1. Don't spam/flood in the chat\n"
+            + "2. Don't insult another players\n"
+            + "3. Don't grief\n"
+            + "[scarlet]4. Don't build NSFW (18+) schemes[]\n";
+
+    public static final String WELCOME_EN = "[white]Hi, traveller!\n"
+            + "Welcome to thedimas' servers!\n"
+            + "Here are the rules:\n"
+            + "[accent]" + RULES_EN + "[]\n"
+            + "\nIf you forgot them, you can type [accent]/rules[] command\n"
+            + "Detailed rules you can get in our Discord server";
+}
