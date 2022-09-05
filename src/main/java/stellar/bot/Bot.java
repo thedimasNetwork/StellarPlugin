49c8df8db11276fc8e92b2238be27bbc53b272d3
thedimas3007 <dima.orischenko@gmail.com>
2022-09-05 21:23:35 +0300

    mindustry -> discord chat

--- src/main/java/stellar/bot/Bot.java
+++ src/main/java/stellar/bot/Bot.java
@@ -1,19 +1,15 @@
 package stellar.bot;
 
 import arc.util.Log;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
-import net.dv8tion.jda.api.utils.messages.MessageCreateData;
+import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.utils.messages.*;
 import net.dv8tion.jda.internal.entities.EntityBuilder;
 
 import javax.security.auth.login.LoginException;
 
 import java.util.Collections;
+import java.util.EnumSet;
 
 import static stellar.Variables.config;
 
@@ -24,13 +20,14 @@ public class Bot {
         try {
             Activity activity = null;
             if (config.bot.main) {
-                activity = EntityBuilder.createActivity("Mindustry на play.thedimas.pp.ua", null, Activity.ActivityType.PLAYING);
+                activity = EntityBuilder.createActivity("Mindustry на play.thedimas.pp.ua", config.discordUrl, Activity.ActivityType.PLAYING);
             }
             jda = JDABuilder.createDefault(config.bot.token) // don't use the deprecated constructor
                     .setActivity(activity)
                     .addEventListeners(new DiscordListener()) // register your listener
                     .build()
                     .awaitReady();
+            MessageRequest.setDefaultMentions(EnumSet.of(Message.MentionType.CHANNEL, Message.MentionType.EMOJI));
             channel = jda.getTextChannelById(config.bot.channelId);
         } catch (InterruptedException | LoginException e) {
             Log.err(e);
@@ -49,7 +46,7 @@ public class Bot {
     public static void sendMessage(String content) {
         MessageCreateData message = new MessageCreateBuilder()
                 .setContent(content)
-                .setAllowedMentions(Collections.emptyList())
+                .setAllowedMentions(Collections.singleton(Message.MentionType.CHANNEL))
                 .build();
         channel.sendMessage(message).queue();
     }
--- src/main/java/stellar/bot/ServerListener.java
+++ src/main/java/stellar/bot/ServerListener.java
@@ -3,16 +3,35 @@ package stellar.bot;
 import arc.Events;
 import arc.util.Log;
 import mindustry.game.EventType;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import stellar.ThedimasPlugin;
 
 public class ServerListener {
     public static void listen() {
         Events.on(EventType.PlayerJoin.class, event -> {
-            Bot.sendMessage(String.format("%s зашел на сервер", ThedimasPlugin.stripColorsAndGlyphs(event.player.name)));
+            String name = ThedimasPlugin.stripColorsAndGlyphs(event.player.name);
+            MessageEmbed embed = new EmbedBuilder()
+                    .setDescription(String.format("%s зашел на сервер", name))
+                    .setColor(0x43D581)
+                    .build();
+            Bot.sendEmbed(embed);
         });
 
         Events.on(EventType.PlayerLeave.class, event -> {
-            Bot.sendMessage(String.format("%s покинул сервер", ThedimasPlugin.stripColorsAndGlyphs(event.player.name)));
+            String name = ThedimasPlugin.stripColorsAndGlyphs(event.player.name);
+            MessageEmbed embed = new EmbedBuilder()
+                    .setDescription(String.format("%s покинул сервер", name))
+                    .setColor(0xF04747)
+                    .build();
+            Bot.sendEmbed(embed);
+        });
+
+        Events.on(EventType.PlayerChatEvent.class, event -> {
+            String name = ThedimasPlugin.stripColorsAndGlyphs(event.player.name);
+            String text = ThedimasPlugin.stripColorsAndGlyphs(event.message);
+            String msg = String.format("**%s**: %s", name, text);
+            Bot.sendMessage(msg);
         });
     }
 }
