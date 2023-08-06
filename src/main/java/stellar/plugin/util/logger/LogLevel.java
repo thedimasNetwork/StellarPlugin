24154f1fff07eb4e4a73cb1da1557cfed7d1c052
thedimas3007 <dima.orischenko@gmail.com>
2023-08-06 23:00:50 +0200

    removed log emoji and changed colors

--- src/main/java/stellar/plugin/util/logger/LogLevel.java
+++ src/main/java/stellar/plugin/util/logger/LogLevel.java
@@ -1,13 +1,15 @@
 package stellar.plugin.util.logger;
 
+import stellar.plugin.bot.Colors;
+
 import java.awt.*;
 
 public enum LogLevel {
 
-    INFO("<:info:871833048956678164> INFO", new Color(0x0028CD)),
-    WARN("<:warn:871831558376530041> WARN", new Color(0xE07400)),
-    ERR("<:error:871832017237573632> ERROR", new Color(0xE50000)),
-    DEBUG("<:debug:871833418319691777> DEBUG", new Color(0x01D90B));
+    INFO("INFO", Colors.blue),
+    WARN("WARN", Colors.yellow),
+    ERR("ERROR", Colors.red),
+    DEBUG("DEBUG", Colors.green);
 
     public final String name;
     public final Color color;
